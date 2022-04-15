7aefebb75a4c2363aa3050cf66b7bea2597a2049
paullete <paulettenereah3@gmail.com>
2022-04-15 20:26:19 +0300

    details

--- git-search/src/app/app.module.ts
+++ git-search/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search/search.component';
 import { UserComponent } from './user/user/user.component';
 import { DateCountPipe } from './date-count.pipe';
+import { HttpClientModule } from '@angular/common/http';
+import { SearchService } from './search.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,11 @@ import { DateCountPipe } from './date-count.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
